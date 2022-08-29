f3223b22a18067624c53735cbe194d42ce203c7b
Андрей <newart525@yandex.ru>
2022-08-29 20:52:47 +0300

    1

--- EX2/EX2.cpp
+++ EX2/EX2.cpp
@@ -2,24 +2,87 @@
 #include <fstream>
 #include <string>
 
-class Contact {
+class Data {
+public:
 	std::string number;
 	std::string name;
 };
 
 class Phone {
+public:
+	Data contact;
 	std::fstream phoneBook;
 	std::string command;
-	std::string input;
+	std::string inNumber;
+	std::string inName;
+
+	void sms() {
+
+	}
+
+	void call() {
+
+	}
 
 	void add() {
 		std::cout << "Enter phone number: ";
 		do {
-			std::cin >> input;
-			if (input[0] != '+' || input[1] != '7' || input.length() != 10) {
+			std::getline(std::cin, inNumber);
+			if (inNumber[0] != '+' || inNumber[1] != '7' || inNumber.length() != 12) {
 				std::cout << "Invalid number format." << std::endl;
 			}
-		} while (input[0] != '+' || input[1] != '7' || input.length() != 10);
-
+		} while (inNumber[0] != '+' || inNumber[1] != '7' || inNumber.length() != 12);
+		std::getline(std::cin, inName);
+		phoneBook.open("phonebook.txt");
+		while (!feof) {
+			phoneBook >> contact.number >> contact.name;
+			if (inNumber == contact.number) {
+				break;
+				std::cout << "This number is already recorded under the name " << contact.name << std::endl;
+				std::cout << "Call this contact or send sms?" << std::endl;
+				do {
+					std::cin >> command;
+					if (command == "sms") {
+						sms();
+					}
+					else if (command == "call") {
+						call();
+					}
+					else {
+						std::cout << "Uncknown command" << std::endl;
+					}
+				} while (command != "sms" || command != "call");
+			}
+			else if (inName == contact.name) {
+				break;
+				do{
+					std::cout << "This name is already recorded with number " << contact.number << std::endl;
+					std::cout << "Coose another name:";
+					std::getline(std::cin, inName);
+				} while (inName == contact.name);
+				std::cin.clear();
+			}
+		}
+		std::cin.clear();
+		phoneBook.close();
+		phoneBook.open("phonebook.txt", std::ios::app);
+		phoneBook << inNumber << " " << inName << std::endl;		
 	}
 };
+
+int main() {
+	Phone phone;
+	do {
+		std::cout << "Command: ";
+		std::cin >> phone.command;
+		if (phone.command == "add") {
+			phone.add();
+		}
+		else if (phone.command == "call") {
+			phone.call();
+		}
+		else if (phone.command == "sms") {
+			phone.sms();
+		}
+	} while (phone.command != "exit");
+}
