006339716dc3620cd32a86e94d23bc15a3043fda
Андрей <newart525@yandex.ru>
2022-08-30 20:59:25 +0300

    1

--- EX2/EX2.cpp
+++ EX2/EX2.cpp
@@ -27,40 +27,50 @@ public:
 	void add() {
 		std::cout << "Enter phone number: ";
 		do {
-			std::getline(std::cin, inNumber);
+			std::cin >> inNumber;
 			if (inNumber[0] != '+' || inNumber[1] != '7' || inNumber.length() != 12) {
 				std::cout << "Invalid number format." << std::endl;
 			}
 		} while (inNumber[0] != '+' || inNumber[1] != '7' || inNumber.length() != 12);
-		std::getline(std::cin, inName);
-		phoneBook.open("phonebook.txt");
-		while (!feof) {
-			phoneBook >> contact.number >> contact.name;
-			if (inNumber == contact.number) {
-				break;
-				std::cout << "This number is already recorded under the name " << contact.name << std::endl;
-				std::cout << "Call this contact or send sms?" << std::endl;
-				do {
-					std::cin >> command;
-					if (command == "sms") {
-						sms();
-					}
-					else if (command == "call") {
-						call();
-					}
-					else {
-						std::cout << "Uncknown command" << std::endl;
-					}
-				} while (command != "sms" || command != "call");
-			}
-			else if (inName == contact.name) {
-				break;
-				do{
-					std::cout << "This name is already recorded with number " << contact.number << std::endl;
-					std::cout << "Coose another name:";
-					std::getline(std::cin, inName);
-				} while (inName == contact.name);
-				std::cin.clear();
+		std::cin.clear();
+		std::cout << "Enter contact name: ";
+		std::getline(std::cin >> std::ws, inName);
+		phoneBook.open("phonebook.txt", std::ios::in);
+		if (phoneBook.is_open()) {
+			std::string line;
+			while (!phoneBook.eof()) {
+				phoneBook >> contact.number >> contact.name;
+				std::cout << contact.number << " | " << contact.name << std::endl;
+				if (inNumber == contact.number) {
+					
+					std::cout << "This number is already recorded under the name " << contact.name << std::endl;
+					std::cout << "Call this contact or send sms?" << std::endl;
+					do {
+						std::cin >> command;
+						if (command == "sms") {
+							sms();
+						}
+						else if (command == "call") {
+							call();
+						}
+						else if (command == "exit") {
+							break;
+						}
+						else {
+							std::cout << "Uncknown command" << std::endl;
+						}
+					} while (command != "sms" || command != "call");
+					break;
+				}
+				else if (inName == contact.name) {
+					do {
+						std::cout << "This name is already recorded with number " << contact.number << std::endl;
+						std::cout << "Coose another name:";
+						std::getline(std::cin, inName);
+					} while (inName == contact.name);
+					std::cin.clear();
+					break;
+				}
 			}
 		}
 		std::cin.clear();
