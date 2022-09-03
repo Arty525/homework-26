c801697af8c969b34171b40c613b005dac1c0828
Андрей <newart525@yandex.ru>
2022-09-03 20:04:58 +0300

    1

---
+++ EX3/EX3.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+
+class Monitor {
+public:
+	std::vector<int> width;
+};
+
+class Window {
+
+};
+
+class Control {
+	
+public:
+	void display(std::vector<Monitor> height) {
+		for (int i = 0; i < 50; ++i) {
+			for (int j = 0; j < 80; ++j) {
+				std::cout << height[i].width[j];
+			}
+		}
+	}
+
+	void command() {
+		std::string com;
+	}
+	
+};
+
+void initialization(std::vector<Monitor> height) {
+	for (int i = 0; i < 50; ++i) {
+		height.push_back(Monitor());
+		for (int j = 0; j < 80; ++j) {
+			height[i].width.push_back(0);
+		}
+	}
+}
+
+int main() {
+	std::vector<Monitor> height;
+	Control control;
+
+	initialization(height);
+
+	control.display(height);
+
+	return 0;
+}
