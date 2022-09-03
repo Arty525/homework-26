60ff6c41155df2fd606b522d00abeb6110ee1100
Андрей <newart525@yandex.ru>
2022-09-03 19:00:47 +0300

    ready

--- EX2/EX2.cpp
+++ EX2/EX2.cpp
@@ -9,19 +9,73 @@ public:
 };
 
 class Phone {
-public:
 	Data contact;
 	std::fstream phoneBook;
 	std::string command;
 	std::string inNumber;
 	std::string inName;
 
-	void sms() {
+	void search() {
+		phoneBook.open("phonebook.txt", std::ios::in);
+		if (phoneBook.is_open()) {
+			while (!phoneBook.eof()) {
+				phoneBook >> contact.number >> contact.name;
+				if (inNumber == contact.number) {
+					std::cout << contact.number << " " << contact.name << std::endl;
+					break;
+				}
+				else if (inName == contact.name) {
+					do {
+						std::cout << "This name is already recorded with number " << contact.number << std::endl;
+						std::cout << "Chose another name:";
+						std::getline(std::cin, inName);
+					} while (inName == contact.name);
+					std::cin.clear();
+					break;
+				}
+			}
+		}
+		std::cin.clear();
+		phoneBook.close();
+	}
 
+	void sms() {
+		std::string text;
+		if (contact.number.empty()) {
+			std::cout << "Enter number: ";
+			std::cin >> inNumber;
+		}
+		search();
+		std::cout << "SMS to ";
+		if (!contact.name.empty()) {
+			std::cout << contact.name << std::endl;
+		}
+		else {
+			std::cout << inNumber << std::endl;
+		}
+		std::cout << "Text: ";
+		std::getline(std::cin >> std::ws, text);
+		std::cout << "SMS sent." << std::endl;
+		contact.number.clear();
+		contact.name.clear();
 	}
 
 	void call() {
-
+		if (contact.number.empty()) {
+			std::cout << "Enter number: ";
+			std::cin >> inNumber;
+		}
+		search();
+		std::cout << "Call to ";
+		if (!contact.name.empty()) {
+			std::cout << contact.name << std::endl;
+		}
+		else {
+			std::cout << inNumber << std::endl;
+		}
+		std::cout << "Call ended." << std::endl;
+		contact.number.clear();
+		contact.name.clear();
 	}
 
 	void add() {
@@ -35,64 +89,47 @@ public:
 		std::cin.clear();
 		std::cout << "Enter contact name: ";
 		std::getline(std::cin >> std::ws, inName);
+		search();
+		phoneBook.open("phonebook.txt", std::ios::app);
+		phoneBook << inNumber << " " << inName << std::endl;
+		phoneBook.close();
+		contact.number.clear();
+		contact.name.clear();
+	}
+
+	void list() {
 		phoneBook.open("phonebook.txt", std::ios::in);
+		std::string line;
 		if (phoneBook.is_open()) {
-			std::string line;
 			while (!phoneBook.eof()) {
-				phoneBook >> contact.number >> contact.name;
-				std::cout << contact.number << " | " << contact.name << std::endl;
-				if (inNumber == contact.number) {
-					
-					std::cout << "This number is already recorded under the name " << contact.name << std::endl;
-					std::cout << "Call this contact or send sms?" << std::endl;
-					do {
-						std::cin >> command;
-						if (command == "sms") {
-							sms();
-						}
-						else if (command == "call") {
-							call();
-						}
-						else if (command == "exit") {
-							break;
-						}
-						else {
-							std::cout << "Uncknown command" << std::endl;
-						}
-					} while (command != "sms" || command != "call");
-					break;
-				}
-				else if (inName == contact.name) {
-					do {
-						std::cout << "This name is already recorded with number " << contact.number << std::endl;
-						std::cout << "Coose another name:";
-						std::getline(std::cin, inName);
-					} while (inName == contact.name);
-					std::cin.clear();
-					break;
-				}
+				std::getline(phoneBook, line);
+				std::cout << line << std::endl;
 			}
 		}
-		std::cin.clear();
-		phoneBook.close();
-		phoneBook.open("phonebook.txt", std::ios::app);
-		phoneBook << inNumber << " " << inName << std::endl;		
+	}
+
+public:
+	void com() {
+		do {
+			std::cout << "Command: ";
+			std::cin >> command;
+			if (command == "add") {
+				add();
+			}
+			else if (command == "call") {
+				call();
+			}
+			else if (command == "sms") {
+				sms();
+			}
+			else if (command == "list") {
+				list();
+			}
+		} while (command != "exit");
 	}
 };
 
 int main() {
 	Phone phone;
-	do {
-		std::cout << "Command: ";
-		std::cin >> phone.command;
-		if (phone.command == "add") {
-			phone.add();
-		}
-		else if (phone.command == "call") {
-			phone.call();
-		}
-		else if (phone.command == "sms") {
-			phone.sms();
-		}
-	} while (phone.command != "exit");
+	phone.com();
 }
