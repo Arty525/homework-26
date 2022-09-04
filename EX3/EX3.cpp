1b3947632a3895133cc62d05ca8fbdae9531ed34
Андрей <newart525@yandex.ru>
2022-09-04 11:35:43 +0300

    ready

--- EX3/EX3.cpp
+++ EX3/EX3.cpp
@@ -7,16 +7,54 @@ public:
 };
 
 class Window {
-
+public:
+	int width;
+	int height;
+	int posx;
+	int posy;
 };
 
 class Control {
-	void move() {
-
+public:
+	void build(std::vector<Monitor>& height, Window& window) {
+		for (int i = 0; i < 50; ++i) {
+			height.push_back(Monitor());
+			for (int j = 0; j < 80; ++j) {
+				if (i >= window.posy && i < window.posy + window.height && j >= window.posx && j < window.posx + window.width) {
+					height[i].width[j] = 1;
+				}
+				else {
+					height[i].width[j] = 0;
+				}
+			}
+		}
 	}
 
-	void resize() {
+private:
+	void move(std::vector<Monitor>& height, Window& window) {
+		do {
+			std::cout << "Input new position. x: ";
+			std::cin >> window.posx;
+			std::cout << "y: ";
+			std::cin >> window.posy;
+			if (window.posx < 0 || window.posx > 79 || window.posy < 0 || window.posy > 49 || window.posx + window.width > 80 || window.posy + window.height > 50) {
+				std::cerr << "Invalid incorrect!" << std::endl;
+			}
+		} while (window.posx < 0 || window.posx > 79 || window.posy < 0 || window.posy > 49 || window.posx + window.width > 80 || window.posy + window.height > 50);
+		build(height, window);
+	}
 
+	void resize(std::vector<Monitor>& height, Window& window) {
+		do {
+			std::cout << "Input new size. Width: ";
+			std::cin >> window.width;
+			std::cout << "height: ";
+			std::cin >> window.height;
+			if (window.posx + window.width > 80 || window.posy + window.height > 80 || window.width <= 0 || window.height <= 0) {
+				std::cerr << "Incorrect size!" << std::endl;
+			}
+		} while (window.posx + window.width > 80 || window.posy + window.height > 80 || window.width <= 0 || window.height <= 0);
+		build(height, window);
 	}
 	
 public:
@@ -29,17 +67,17 @@ public:
 		}
 	}
 
-	void command(std::vector<Monitor>& height) {
+	void command(std::vector<Monitor>& height, Window& window) {
 		std::string com;
 		do {
 			std::cout << "Command:";
 			std::cin >> com;
 
 			if (com == "move") {
-				move();
+				move(height, window);
 			}
 			else if (com == "resize") {
-				resize();
+				resize(height, window);
 			}
 			else if (com == "display") {
 				display(height);
@@ -47,30 +85,37 @@ public:
 			else if (com == "close") {
 				std::cout << "Window close" << std::endl;
 			}
+			else {
+				std::cout << "Unknown command!" << std::endl;
+			}
 		} while (com != "close");
 	}
 };
 
-void initialization(std::vector<Monitor>& height) {
-	for (int i = 0; i < 50; ++i) {
-		height.push_back(Monitor());
-		for (int j = 0; j < 80; ++j) {
-			height[i].width.push_back(0);
-		}
-	}
-}
+
 
 int main() {
 	std::vector<Monitor> height;
 	Control control;
+	Window window;
+	window.height = 10;
+	window.width = 10;
+	window.posx = 33;
+	window.posy = 18;
 
-	initialization(height);
+	height.resize(50);
+	for (int i = 0; i < 50; ++i) {
+		height[i].width.resize(80);
+	}
+
+	control.build(height, window);
 
 	control.display(height);
 
-	std::cout << std::endl;
+	std::cout << "Current size: " << window.width << "x" << window.height << std::endl;
+	std::cout << "Current position: (" << window.posx << ":" << window.posy << ")" << std::endl;
 
-	control.command(height);
+	control.command(height, window);
 
 	return 0;
 }
